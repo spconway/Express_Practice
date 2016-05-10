c984f4eb75431ee745237b29b9905e5f9965075d
Stephen Conway <s_conway1@post.massbay.edu>
2016-05-10 12:17:41 -0400

    Removed socket.io code as a separate module and moved it into the route page to avoid multiple connects and multiple sends. Also added various style changes and minor changes to socket.io code for form submit and displaying user name when a user sends a message.

--- public/javascripts/socketio.js
+++ public/javascripts/socketio.js
@@ -1,4 +1,40 @@
 $(function(){
+    // curUser = token from backend
     var socket = io();
-    console.log(socket);
+    var messageCodes = {
+        global: 'global',
+        private: 'private'
+    };
+    var msg = $('#messageInput');
+    var messages = $('#messageList');
+
+    // on form submit check for empty string
+    // then send global message
+    $("form").submit(function(e) {
+        if (msg.val() !== "") {
+            socket.emit(messageCodes.global, {message: msg.val(), user: curUser.user_name});
+            msg.val("");
+        }
+        return false;
+    });
+
+    // handle socket response message
+    socket.on(messageCodes.global, function(data){
+        console.log(data);
+        return messageFormat(data.message, data.user);
+    });
+
+    // format message to display username nicely
+    function messageFormat(message, userName){
+        var style = userName === curUser.user_name ? 'myMessage' : 'notMyMessage';;
+        var col;
+        var uname;
+        var text = $('<li>', {class: 'text-left'}).text(message);
+
+        col = $('<div>', {class: 'col-md-12 col-sm-12 ' + style});
+        uname = $('<div>', {class: 'col-md-12 col-sm-12 ' + style + ' username'});
+        uname.append($('<li>', {class: 'text-left'}).text(userName));
+        col.append(uname).append(text);
+        messages.append(col);
+    }
 });
--- routes/socketio.js
+++ routes/socketio.js
@@ -7,22 +7,44 @@
  * object I can inject socket io into whatever route
  * I want.
  */
+
+// globals for caching information when users navigate
+// away from the chat page but try to come back
+var people = {};
+var rooms = {};
+var sockets = [];
+var chatHistory = {};
+
 module.exports = function(io){
     var express =     require('express');
     var router =      express.Router();
-    var log =         require('../logger');
     var requireLogin =  require('../requireLogin');
+    // require room create new room objects
+    var room = require('../room');
+    var user;
 
     /* Handle logout request. Redirect to login page */
     router.get('/', requireLogin, function(req, res) {
+        // assign user when accessing chat page
+        user = req.session.user;
+        user.password = null;
+        // add user to people. Set to user name for now
+        people[user.user_name] = user.user_name;
         res.render('socketio', {
-            title: 'Socket.IO Stuff'
+            title: 'Socket.IO Stuff',
+            user: user
         });
     });
 
-    // io handler stuff
-    io.on('connection', function(socket){
-        console.log('Someone connected! ');
+    io.on('connection', function (socket) {
+        //socket.emit('user joined', {message: (user.user_name + ' has joined.')});
+
+        socket.on('global', function (data) {
+            io.emit('global', {message: data.message, user: data.user});
+        });
+        socket.on('disconnect', function(){
+            console.log('user disconnected');
+        });
     });
 
     // return router for export
